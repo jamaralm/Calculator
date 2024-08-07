b9099e551cbf66cbd06d4dae7436799eee00956c
Joao <joaoamaraloficial07@gmail.com>
2024-08-07 16:29:51 -0300

    Implementação das funções DIVISAO e MULTIPLICACAO.

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,14 +10,14 @@ public class Calculator {
         do {
 
             //Apresenta as operaçoes existentes ao usuario
-            System.out.println("Operações: \n 1 - Soma \n 2 - Subtração\n 3 - Sair =(");
+            System.out.println("Operações: \n 1 - Soma \n 2 - Subtração\n 3 - Divisão \n 4 - Multiplicação \n 5 - Sair =(\n *Apenas numeros Inteiros.");
 
-            System.out.println("Digite a operação desejada\n");
+            System.out.println("Digite a operação desejada: ");
             Integer operationChoice = sc.nextInt();
 
             choiceResult(operationChoice);
 
-            if(operationChoice == 3){
+            if(operationChoice == 5){
                 System.out.print("Sistema Encerrado.");
                 break;
             };
@@ -56,6 +56,22 @@ public class Calculator {
 
                 return sub(firstNumber, secondNumber);
 
+            case 3:
+                System.out.println("Digite o primeiro numero: \n");
+                firstNumber = sc.nextInt();
+                System.out.println("Digite o segundo numero: \n");
+                secondNumber = sc.nextInt();
+
+                return div(firstNumber, secondNumber);
+
+            case 4:
+                System.out.println("Digite o primeiro numero: \n");
+                firstNumber = sc.nextInt();
+                System.out.println("Digite o segundo numero: \n");
+                secondNumber = sc.nextInt();
+
+                return mult(firstNumber, secondNumber);
+
             default:
                 return 0;
         }
@@ -72,6 +88,18 @@ public class Calculator {
         int sub = x - y;
         System.out.println("SUBTRACAO: " + sub);
         return sub;
+    }
+
+    static int div(int x, int y){
+        int div = x / y;
+        System.out.println("DIVISAO: " + div);
+        return div;
+    }
+
+    static int mult(int x, int y){
+        int mult = x * y;
+        System.out.println("MULTIPLICACAO: " + mult);
+        return mult;
     }
 
 }
